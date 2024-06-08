3cf90d6baf45e2521414b039031e93ef2cc1a8bb
manhellis <manh@manhellis.com>
2024-06-08 15:09:22 -0700

    new db

--- classlist.sql
+++ classlist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jun 06, 2024 at 08:51 PM
+-- Generation Time: Jun 08, 2024 at 10:08 PM
 -- Server version: 5.7.39
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,37 @@ SET time_zone = "+00:00";
 --
 -- Database: `classlist`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `new_students`
+--
+
+CREATE TABLE `new_students` (
+  `id` int(8) NOT NULL,
+  `fn` varchar(16) NOT NULL,
+  `ln` varchar(16) NOT NULL,
+  `photo` varchar(64) DEFAULT NULL,
+  `term` int(1) DEFAULT NULL,
+  `subject` varchar(16) DEFAULT NULL,
+  `memory` varchar(256) DEFAULT NULL,
+  `postgrad` varchar(256) DEFAULT NULL,
+  `quote` varchar(256) DEFAULT NULL,
+  `portfolio` varchar(64) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `new_students`
+--
+
+INSERT INTO `new_students` (`id`, `fn`, `ln`, `photo`, `term`, `subject`, `memory`, `postgrad`, `quote`, `portfolio`, `password`, `email`) VALUES
+(1, 'manh', 'ellis', 'cd08920fddb98e53fd065bfd7504c899.jpeg', 4, 'mikescripts', 'nothing', 'job i hope', 'get me out', 'www.manhellis.com', '$2y$10$SfhYIDtn.iOuCW7zfoFLuuZHX6lja4lF4XA4JqNmpiH/.P3zB8JCa', 'test@test.com'),
+(2, 'john smuth', 'test@bcit.ca', '', 1, 'masdf', 'asdf', 'undefined', 'asdasd', '.com', '$2y$10$s10BRAv5LrkiStIRNlVsGOYmbA.1jL5eBACvhcP8hhMiPGHKignTm', 'test@bcit.ca'),
+(3, 'lexi', 'dugo', '57c1844cae8adf0ac29d809a9e370020.gif', 4, 'math', 'boba', 'undefined', 'winning', 'lexidugo.com', '$2y$10$3PQuDBpIHwbFlIksTvRfOuGk3nD37pY84A3BSNNlTCzTgFNmD0erO', 'lexi@bcit.ca'),
+(4, 'new', 'me', '', 4, '123', '123123', 'undefined', '12515', 'wwww', '$2y$10$1lfbk5zOSaZtm2HOIPPPdeIyrErQVFtSW2B/nLK04uZpBJOi3fX0.', 'newme@bcit.ca');
 
 -- --------------------------------------------------------
 
@@ -89,6 +120,12 @@ INSERT INTO `students_login` (`id`, `fn`, `ln`, `photo`, `job`, `words`, `inspir
 --
 
 --
+-- Indexes for table `new_students`
+--
+ALTER TABLE `new_students`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
@@ -103,6 +140,12 @@ ALTER TABLE `students_login`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `new_students`
+--
+ALTER TABLE `new_students`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `students`
